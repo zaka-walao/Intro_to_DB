375a5b9e3b1048a21c25058620ebc8e0b8119b8a
zak mari <marioush741@gmail.com>
2024-07-28 18:46:17 +0100

    update

---
+++ task_2.sql
@@ -0,0 +1,48 @@
+USE alx_book_store;
+
+-- Create table for Authors
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT AUTO_INCREMENT,
+    author_name VARCHAR(215),
+    PRIMARY KEY (author_id)
+);
+
+-- Create table for Books
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Create table for Customers
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
+    PRIMARY KEY (customer_id)
+);
+
+-- Create table for Orders
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create table for Order Details
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
